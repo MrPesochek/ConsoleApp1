8007d26ed9eac76250c38e8cbc3e1385152ddcc3
Kazuma <maxim.peschanyy@gmail.com>
2023-01-23 14:52:26 +0300

    я не знаю как эту боль преодолеть

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MatrixA(10, 12);
         }
-        public static string GdeDesign(string Anton)
+        public static void MatrixA(int a, int b)
         {
-            return "Скоро скину";
+            string[,] matrix = new string[a,b];
+            Random rnd = new Random();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    matrix[i, j] = rnd.Next(2).ToString();
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write('\n');
+            }
+            Console.ResetColor();
         }
     }
 }
