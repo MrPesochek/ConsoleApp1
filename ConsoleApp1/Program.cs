b80fd54adb0a0e23ca679b0433c7c5f0ba98d230
Kazuma <maxim.peschanyy@gmail.com>
2023-01-23 15:17:38 +0300

    q

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,27 +13,9 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Fomin Daniil Antonovich\n10.3\nJuly, 28");
         }
-        public static void MatrixA(int a, int b)
-        {
-            string[,] matrix = new string[a, b];
-            Random rnd = new Random();
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            for (int i = 0; i < a; i++)
-            {
-                for (int j = 0; j < b; j++)
-                {
-                    matrix[i, j] = rnd.Next(2).ToString();
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.Write('\n');
-            }
-            Console.ResetColor();
-        }
 
         static void Main(string[] args)
         {
-            MatrixA(10, 12);
-            MatrixB(10, 12);
             PrintMyName();
         }
     }
