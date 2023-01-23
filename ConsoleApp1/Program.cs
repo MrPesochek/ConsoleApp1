320708d88a5d0bbc40bf35300c50ff8c8cd56fc1
dreamm <Fort-med@yandex.ru>
2023-01-23 14:53:34 +0300

    qwe111

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,13 +4,26 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public static void MatrixB(int n, int h)
+        {
+            int[,] matrix = new int[n, h];
+            Random rnd = new Random();
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < h; j++)
+                {
+                    matrix[i, j] = rnd.Next(2);
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.Write('\n');
+            }
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-        public static string GdeDesign(string Anton)
-        {
-            return "Скоро скину";
+            MatrixB(10, 12);
         }
     }
 }
