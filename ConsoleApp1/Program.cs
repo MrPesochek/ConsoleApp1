8e2ad779e90437a26a9dacdd3d3d9199246e51c5
dreamm <Fort-med@yandex.ru>
2023-01-23 15:15:31 +0300

    ggg

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,22 +4,6 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public static void MatrixB(int n, int h)
-        {
-            int[,] matrix = new int[n, h];
-            Random rnd = new Random();
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < h; j++)
-                {
-                    matrix[i, j] = rnd.Next(2);
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write($"{matrix[i, j]} ");
-                }
-                Console.Write('\n');
-            }
-            Console.ResetColor();
-        }
         public static void PrintMyName()
         {
             Console.WriteLine("Песчаный Максим Кириллович 1000.-7.=993");
@@ -49,7 +33,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             MatrixA(10, 12);
-            MatrixB(10, 12);
             PrintMyName();
         }
     }
