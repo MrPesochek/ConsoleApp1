b9a79b427db7d860a4e394e353b9f0d66ecaf3f9
dreamm <Fort-med@yandex.ru>
2023-01-23 15:19:17 +0300

    111

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,29 +11,11 @@ namespace ConsoleApp1
 
         public static void PrintmyName()
         {
-            Console.WriteLine("Fomin Daniil Antonovich\n10.3\nJuly, 28");
-        }
-        public static void MatrixA(int a, int b)
-        {
-            string[,] matrix = new string[a, b];
-            Random rnd = new Random();
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            for (int i = 0; i < a; i++)
-            {
-                for (int j = 0; j < b; j++)
-                {
-                    matrix[i, j] = rnd.Next(2).ToString();
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.Write('\n');
-            }
-            Console.ResetColor();
+            Console.WriteLine("Fomin Daniil Antonovich\n993-7=986");
         }
 
         static void Main(string[] args)
         {
-            MatrixA(10, 12);
-            MatrixB(10, 12);
             PrintMyName();
         }
     }
