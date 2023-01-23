d065987a022911e02cb063f388ee75f56c91d48f
dreamm <Fort-med@yandex.ru>
2023-01-23 14:56:32 +0300

    сюда

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,24 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public static void MatrixA(int a, int b)
+        {
+            string[,] matrix = new string[a, b];
+            Random rnd = new Random();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    matrix[i, j] = rnd.Next(2).ToString();
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write('\n');
+            }
+            Console.ResetColor();
+        }
         public static void MatrixB(int n, int h)
         {
-            MatrixA(10, 12);
             int[,] matrix = new int[n, h];
             Random rnd = new Random();
             for (int i = 1; i < n; i++)
@@ -21,24 +36,10 @@ namespace ConsoleApp1
             }
             Console.ResetColor();
         }
-        public static void MatrixA(int a, int b)
-        {
-            string[,] matrix = new string[a,b];
-            Random rnd = new Random();
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            for (int i = 0; i < a; i++)
-            {
-                for (int j = 0; j < b; j++)
-                {
-                    matrix[i, j] = rnd.Next(2).ToString();
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.Write('\n');
-            }
-            Console.ResetColor();
 
         static void Main(string[] args)
         {
+            MatrixA(10, 12);
             MatrixB(10, 12);
         }
     }
